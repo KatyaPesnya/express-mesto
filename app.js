3d88dc2c264f093f6244f0177e9840e5eaaa5894
katya pesnya <katyapesnya@icloud.com>
2021-07-03 19:21:09 +0300

    connect mongo

---
+++ app.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-underscore-dangle */
+const express = require('express');
+
+const mongoose = require('mongoose');
+
+const app = express();
+const { PORT = 3000 } = process.env;
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`App listening on port ${PORT}`);
+});
